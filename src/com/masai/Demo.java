a86b04b30633dbdf6a2307cb1d82e03628e4048e
anupgautam23 <anupgautam97@gmail.com>
2022-08-10 23:03:07 +0530

    java revision demo file

--- src/com/masai/Demo.java
+++ src/com/masai/Demo.java
@@ -2,7 +2,7 @@ package com.masai;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("helllo world");
+		System.out.println("helllo this is forst java revision demo file ");
 	}
 
 }
